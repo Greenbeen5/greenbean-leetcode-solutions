9b1114a3ac8f8e3601d48d7224101a8d32f10fcc
greenbeen5 <greenbeen5@outlook.com>
2020-04-01 23:07:52 +0800

    Copied from offical solution

---
+++ solutions/Solution-1111.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} seq
+ * @return {number[]}
+ */
+var maxDepthAfterSplit = function(seq) {
+    let dep = 0;
+    return seq.split("").map((value, index) => {
+        if (value === '(') {
+            ++dep;
+            return dep % 2;
+        } else {
+            let ans = dep % 2;
+            --dep;
+            return ans;
+        }
+    });
+};
