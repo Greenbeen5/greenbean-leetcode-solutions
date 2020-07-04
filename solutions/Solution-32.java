e3b6159ce62812b6cdce84cadc868c2df7d84c7a
greenbeen5 <greenbeen5@outlook.com>
2020-07-04 10:22:27 +0800

    commented the official solution, consider DP

---
+++ solutions/Solution-32.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+        // This is similar to my answer, however left-to-right scanning will not
+        // get the correct result if the string is like "(()", so there should be
+        // a reversed scanning as follows.
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+        
+
+        return maxlength;
+    }
+}
