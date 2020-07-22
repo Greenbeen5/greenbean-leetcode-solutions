941a52a6baf2c0c51618aac522a5463d7aae1ab3
greenbeen5 <greenbeen5@outlook.com>
2020-07-22 11:30:09 +0800

    linear search

---
+++ solutions/Solution-154.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minArray(int[] numbers) {
+        if(numbers.length == 1) return numbers[0];
+        for(int i = 1; i<numbers.length; ++i){
+            if(numbers[i] < numbers[i-1]) return numbers[i];
+        }
+        return numbers[0]; // all the same number
+    }
+}
