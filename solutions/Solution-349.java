cb6b8d9e11970e3c953b999d7c0721a177c7c3dc
greenbeen5 <greenbeen5@outlook.com>
2020-11-02 14:06:29 +0800

    a usage of Set<E>

---
+++ solutions/Solution-349.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums1){
+            set.add(i);
+        }
+        Set<Integer> set2 = new HashSet<>();
+        for(int i : nums2){
+            if(set.contains(i) && !set2.contains(i)){
+                set2.add(i);
+            }
+        }
+        int[] res = new int[set2.size()];
+        int idx = 0;
+        for(int i : set2){
+            res[idx++] = i;
+        }
+        return res;
+    }
+}
